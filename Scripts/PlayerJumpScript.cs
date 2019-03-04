be04ea7e47d56c70b80a92b906a09b09c265948b
vintisachdeva <vintisachdeva17@gmail.com>
2019-03-04 23:49:14 +0530

    Add files via upload

---
+++ Scripts/PlayerJumpScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpScript : MonoBehaviour {
+
+	Rigidbody2D rigidBody2D;
+	private float forwardForce = 0.2f;
+	private float jumpForce = 8f;
+
+	void Start () 
+	{
+		rigidBody2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update () {
+		
+	}
+
+	public void jump()
+	{
+			rigidBody2D.velocity = new Vector2 (forwardForce, jumpForce);
+	}
+}
---
+++ Scripts/PlayerRunScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRunScript : MonoBehaviour {
+
+	public float speed = 5f;
+	bool movingRight = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (movingRight) 
+		{
+			transform.Translate (Vector3.right * speed * Time.deltaTime);
+			GetComponent<SpriteRenderer> ().flipX = false;
+		} 
+		else {
+			transform.Translate (Vector3.left * speed * Time.deltaTime);
+			GetComponent<SpriteRenderer> ().flipX = true;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision2D){
+	
+		if (collision2D.gameObject.tag == "Boundary") 
+		{
+			movingRight = !movingRight;
+		}
+	}
+
+}
